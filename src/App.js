3d5bd8cdc5d601e84a5104d5e9e3d37478d73654
Junaid Farooq <junaid@evercam.io>
2020-02-24 18:25:02 +0500

    Start fetching data from API.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FlightDetails from "./components/FlightDetails";
 
-function App() {
-  return (
-    <div>
-      <FlightDetails />
-    </div>
-  );
+import axios from 'axios';
+
+class App extends Component {
+
+  state = {
+    flightDetails: []
+  }
+
+  callAPI() {
+    axios.get(`http://localhost:5000/flight-details`)
+      .then(res => {
+        let { flightDetails } = res.data
+        this.setState({ flightDetails })
+      })
+  }
+
+  componentDidMount() {
+    this.callAPI()
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.flightDetails.map((fd, i) =>
+          <FlightDetails data={fd} key={i} />
+          )
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
